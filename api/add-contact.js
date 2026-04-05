fe57f3f6a8e232d6504167262377c7a35cf7500d
tomthompson321-pixel <tomthompson321@gmail.com>
2026-04-05 08:55:44 -0700

    Update add-contact.js

--- api/add-contact.js
+++ api/add-contact.js
@@ -1,4 +1,4 @@
-⏺ export default async function handler(req, res) {                             
+export default async function handler(req, res) {                             
     // CORS headers - allow scanner domains
     res.setHeader('Access-Control-Allow-Origin', '*')                           
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')              
